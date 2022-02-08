afa0b50bb75e10186d3f046ed4bdefca79cd79b4
Pablo Montenegro <montenegro.pablo@outlook.com>
2022-02-08 19:58:25 +0100

    add react-ace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+
+import styles from './app.module.css';
+
+function onChange(newValue) {
+    console.log("change", newValue);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AceEditor
+          className={styles.editor}
+          style={{}}
+          width="100%"
+          placeholder="Placeholder Text"
+          mode="javascript"
+          theme="monokai"
+          name="blah2"
+          onChange={onChange}
+          fontSize={14}
+          showPrintMargin={false}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={`function onLoad(editor) {
+  console.log("i've loaded");
+}`}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+      />
     </div>
   );
 }
