f46519a701c7a61ba1e73f0d3242e9233a906ac9
Pablo Montenegro <montenegro.pablo@outlook.com>
2022-02-12 15:34:37 +0100

    add file selection

--- src/App.js
+++ src/App.js
@@ -1,42 +1,139 @@
 import AceEditor from "react-ace";
+import {useCallback, useMemo, useState} from "react";
+
+// Vendor styles
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
 
+// Styles
 import styles from './app.module.css';
 
-function onChange(newValue) {
-    console.log("change", newValue);
-}
+// Components
+import Files from "./Files/Files";
+
+const scale = 1;
+
+const getLanguageFromFilename = (fileName) => {
+    if (fileName.endsWith('.js')) {
+        return 'javascript';
+    }
+
+    if (fileName.endsWith('.html')) {
+        return 'html';
+    }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <AceEditor
-          className={styles.editor}
-          style={{}}
-          width="100%"
-          placeholder="Placeholder Text"
-          mode="javascript"
-          theme="monokai"
-          name="blah2"
-          onChange={onChange}
-          fontSize={14}
-          showPrintMargin={false}
-          showGutter={true}
-          highlightActiveLine={true}
-          value={`function onLoad(editor) {
-  console.log("i've loaded");
-}`}
-          setOptions={{
-            enableBasicAutocompletion: false,
-            enableLiveAutocompletion: false,
-            enableSnippets: false,
-            showLineNumbers: true,
-            tabSize: 2,
-          }}
-      />
-    </div>
-  );
+    const [files, setFiles] = useState([{ name: 'untitled.js', isSelected: true, content: '' }]);
+    const [currentFileIndex, setCurrentFileIndex] = useState(0);
+
+    const onChange = useCallback((content) => {
+        setFiles([
+            ...files.map((file, idx) => {
+                if (currentFileIndex === idx) {
+                    return {
+                        ...file,
+                        content,
+                    };
+                }
+
+                return file;
+            }),
+        ]);
+    }, [currentFileIndex, files]);
+
+    const currentFile = useMemo(() => {
+        return {
+            ...files[currentFileIndex],
+            mode: getLanguageFromFilename(files[currentFileIndex].name),
+        };
+    }, [currentFileIndex, files]);
+
+    return (
+        <div
+            className={styles.container}
+            style={{
+                width: `${960 * scale}px`,
+                height: `${540 * scale}px`,
+            }}
+        >
+            <div
+                style={{
+                    width: '50%',
+                    backgroundColor: '#2f3129',
+                }}
+            >
+                <Files
+                    files={files}
+                    onEditName={(newName, index) => {
+                        if (newName) {
+                            setFiles([
+                                ...files.map((file, idx) => {
+                                    if (idx === index) {
+                                        return {
+                                            ...file,
+                                            name: newName,
+                                        };
+                                    }
+
+                                    return file;
+                                }),
+                            ]);
+                        }
+                    }}
+                    onSelectFile={(index) => {
+                        setCurrentFileIndex(index);
+                        setFiles([
+                            ...files.map((file, idx) => {
+                                return {
+                                    ...file,
+                                    isSelected: idx === index,
+                                };
+                            }),
+                        ]);
+                    }}
+                    onAddMore={() => {
+                        setCurrentFileIndex(files.length);
+                        setFiles([
+                            ...files.map((file) => {
+                                return {
+                                    ...file,
+                                    isSelected: false,
+                                };
+                            }),
+                            { name: `untitled-${files.length}.js`, isSelected: true, content: '' }
+                        ]);
+                    }}
+                />
+                <AceEditor
+                    className={styles.editor}
+                    style={{}}
+                    width="100%"
+                    height="100%"
+                    placeholder="// start typing"
+                    mode={currentFile.mode}
+                    theme="monokai"
+                    name="blah2"
+                    onChange={onChange}
+                    fontSize={20 * scale}
+                    showPrintMargin={false}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={currentFile.content}
+                    setOptions={{
+                        enableBasicAutocompletion: false,
+                        enableLiveAutocompletion: false,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                        tabSize: 2,
+                    }}
+                />
+            </div>
+            <div>
+                TODO
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Files/Files.js
@@ -0,0 +1,53 @@
+import classNames from "classnames";
+
+import styles from './files.module.css';
+
+function Files({
+    files = [],
+    onAddMore,
+    onEditName,
+    onSelectFile,
+}) {
+    return (
+        <ul className={styles['list-wrapper']}>
+            {files.map((file, index) => {
+                return (
+                    <li
+                        key={file.name}
+                        onBlur={(event) => {
+                            const element = event.target;
+                            element.contentEditable = false;
+                            onEditName(event?.currentTarget?.textContent, index);
+                        }}
+                        // contentEditable={true}
+                        onClick={(event) => {
+                            const element = event.target;
+                            element.contentEditable = true;
+                            setTimeout(function() {
+                                if (document.activeElement !== element) {
+                                    element.contentEditable = false;
+                                    onSelectFile(index);
+                                }
+                            }, 300);
+                        }}
+                        spellCheck={false}
+                        className={classNames(styles['list-item'], {
+                            [styles['selected-list-item']]: file.isSelected,
+                        })}
+                    >
+                        {file.name}
+                    </li>
+                );
+            })}
+            <li
+                key="add-more"
+                className={styles['list-item']}
+                onClick={onAddMore}
+            >
+                +
+            </li>
+        </ul>
+    );
+}
+
+export default Files;
